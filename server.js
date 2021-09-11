5b664161630cf55524e3435e5495c32d01d51662
lohith <lohith05amal@gmail.com>
2021-09-11 03:52:28 -0400

    add heroku

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const {animals} = require('./data/animal.json');
 
+const PORT = process.env.PORT || 3001;
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   // Note that we save the animalsArray as filteredResults here:
@@ -51,5 +53,7 @@ app.get('/api/animals', (req,res)=> {
   res.json(results);
 });
 
-app.listen(3001);
+app.listen(PORT, ()=>{
+  console.log(`API server now on port ${PORT}!`);
+});
 
